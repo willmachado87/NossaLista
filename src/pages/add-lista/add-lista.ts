b8e43b25020139ce8cd6bb4a20ead0e6b91b93c6
willmachado87 <will.machado87@gmail.com>
2018-11-08 01:20:07 -0300

    validações de campos
    validações de campos e refatorisação de refactorização

--- src/pages/add-lista/add-lista.ts
+++ src/pages/add-lista/add-lista.ts
@@ -1,23 +1,10 @@
+import { Util } from './../util';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ToastController, LoadingController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import * as firebase from 'firebase';
 import { Observable, Subscription } from 'rxjs';
-
-
-export class Lista {
-
-  nome_lista: String;
-  itens;
-  usuarios;
-
-  constructor(nome_lista: string, usuarios: string[]) {
-    this.nome_lista = nome_lista;
-    this.itens = [];
-    this.usuarios = usuarios;
-  }
-}
-//----------------------------------------------------------------
+import { Lista, User } from '../model';
 
 @IonicPage()
 @Component({
@@ -25,155 +12,111 @@ export class Lista {
   templateUrl: 'add-lista.html',
 })
 export class AddListaPage {
+  user: any;
+  email: string;
+  listTemp = new Array();
+  nameList: string;
   editList: boolean;
-  usuario: any;
-  nome_lista: string;
-  idLista: string
-  email: string = null;
-
-  lista;
-  listaUsuarios = new Array;
-  localiza: Observable<any[]>;
-  usuariosFull = new Array;
+  idList: string   
+  listDoc:any;  
+  userList = new Array;    
   subscr: Subscription;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,
-    public bd: AngularFirestore, public toastCtrl: ToastController,
-    public loadingCtrl: LoadingController) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, 
+              public bd: AngularFirestore, public util: Util){}
 
-  ionViewDidLoad() {
+  ionViewDidLoad() {       
     this.editList = this.navParams.get("editList");
-    this.idLista = this.navParams.get("idLista");
-    if (this.editList == false || undefined) {
-      this.usuario = firebase.auth().currentUser;
-      this.listaUsuarios.push({ nomeDisplay: "", email: this.email, id: null });
-    } else {
-      this.lista = this.bd.doc('listas/' + this.idLista).valueChanges();
-      this.subscr = this.lista.subscribe(dataFs => {
-        dataFs.usuarios.forEach(date => {
+    this.idList = this.navParams.get("idLista");
+    this.user = firebase.auth().currentUser;
+    if(this.editList == true) {
+      this.listDoc = this.bd.doc('listas/' + this.idList).valueChanges();
+      this.subscr = this.listDoc.subscribe( dataFs => {
+        this.nameList = dataFs.nome_lista;
+        dataFs.usuarios.forEach( date => {
           let temp = this.bd.doc('usuarios/' + date).valueChanges();
-          temp.subscribe(w => { this.usuariosFull.push(w); });
+          temp.subscribe( date2 => { this.userList.push(date2); });
         });
-      });
+      });      
     }
   }
-
+  
   ngOnDestroy() {
-    if (this.editList == true) {
+    if(this.editList == true) {
       this.subscr.unsubscribe();
     }
   }
 
-  deleteList() {
-    //this.navCtrl.setRoot(RedirectPage,{idListaDel:this.idLista});          
-  }
-
-
-  validUser(valorInput, ind) {
-    let referencia = this.bd.collection("usuarios", ref => {
-      return ref.where('email', '==', valorInput);
+  btAddUser(){    
+    let ref = this.bd.collection("usuarios", queryFS => {
+      return queryFS.where('email', '==', this.email)
     });
-    this.localiza = referencia.valueChanges();
-    this.localiza.subscribe(doc => {
-      if (doc.length > 0) {
-        this.usuariosFull.splice(ind, 0, doc[0]);
-        this.usuariosFull.pop();
-        console.log(this.usuariosFull);
-        this.toastVier("Usuario " + doc[0].nomeDisplay + " Encontrado", 3000);
-      } else {
-        this.usuariosFull.pop();
-        this.toastVier("Usuario não encontrado/cadastrado", 3000);
+    let find: Observable<any> = ref.valueChanges();     
+    find.subscribe(doc => {
+      if(doc.length > 0) {
+        this.listTemp.push({ nomeDisplay: doc[0].nomeDisplay, email: doc[0].email, id: doc[0].id });        
+        this.util.showToast("Usuario " + doc[0].nomeDisplay + " Encontrado com sucesso", "bottom", 3000);
+        console.log(this.listTemp);
+        this.email = null;       
+      }else{       
+        this.util.showToast("Usuario não Cadastrado", "bottom", 3000);        
+      }
+    });    
+  }
+  
+  btAddUserEdit(){
+    let ref = this.bd.collection("usuarios", queryFS => {
+      return queryFS.where('email', '==', this.email)
+    });
+    let find: Observable<any> = ref.valueChanges();     
+    find.subscribe(doc => {
+      if(doc.length > 0) {
+        this.userList.push({ nomeDisplay: doc[0].nomeDisplay, email: doc[0].email, id: doc[0].id });        
+        this.util.showToast("Usuario " + doc[0].nomeDisplay + " Encontrado com sucesso", "bottom", 3000);        
+        this.email = null;       
+      }else{       
+        this.util.showToast("Usuario não Cadastrado", "bottom", 3000);        
       }
     });
-  }
-
-  //salvar Edição feita
-  saveEdit() {
-    let refdoc = this.bd.collection('listas');
-    refdoc.doc(this.idLista).ref.get().then(c => {
-      if (c.exists) {
-        let arryTemp = new Array;
-        this.usuariosFull.forEach(element => {
-          arryTemp.push(element.id);
-        });
-        refdoc.doc(this.idLista).update({ usuarios: arryTemp });
-        this.toastVier("Lista Atualizada com Sucesso", 3000);
-        this.navCtrl.pop();
-      } else {
-        this.toastVier("Usuario não Encontrado o Banco de dados", 3000);
-        this.navCtrl.pop();
-      }
-    });
-  }
-
-  //add novo usuario 
-  addNewUser() {
-    if (this.editList == true) {
-      this.usuariosFull.push({ email: this.email });
-    } else {
-      this.listaUsuarios.push({ email: this.email });
-    }
-
-  }
-  //remove usuario da lista
-  removeUserList(user: any, index: number) {
-    if (this.editList == true) {
-      this.usuariosFull.splice(index, 1);
-      console.log("Lista full", this.usuariosFull);
-    } else {
-      this.listaUsuarios.splice(index, 1);
+  } 
+  
+  addListBd(){
+    if(this.editList == true){
+      let listaComIds = new Array();
+      this.userList.forEach(data => {
+        listaComIds.push(data.id);
+      });      
+      this.bd.collection('listas').doc(this.idList).update({nome_lista:this.nameList, usuarios:listaComIds});
+      this.util.showToast("Lista Editada com Sucesso", "bottom", 3000);
+      this.navCtrl.pop();
+    }else{
+      let userConnected = new User(this.user.uid, this.user.displayName, this.user.email);
+      this.listTemp.push(userConnected);      
+      let listaComIds = new Array();
+      this.listTemp.forEach(data => { listaComIds.push(data.id); });
+      let list = new Lista(this.nameList, listaComIds);    
+      this.bd.collection('listas').doc(this.bd.createId()).set(Object.assign({}, list));
+      this.util.showToast("Lista Criada com Sucesso", "bottom", 3000);
+      this.navCtrl.pop();  
     }
   }
 
-  //add outro usuario na lista de usuarios
-  addOutroUsuario(obj: any, indice: number) {
-    let referencia = this.bd.collection("usuarios", ref => {
-      return ref.where('email', '==', obj.email)
-    });
-    this.localiza = referencia.valueChanges();
-    this.localiza.subscribe(doc => {
-      if (doc.length > 0) {
-        this.listaUsuarios.splice(indice, 1, { nomeDisplay: doc[0].nomeDisplay, email: doc[0].email, id: doc[0].id });
-        //let novoItem = {nomeDisplay:"", email: this.email, id: ""};    
-        //this.listaUsuarios.push(novoItem);
-        this.toastVier("Usuario " + doc[0].nomeDisplay + " Encontrado", 3000);
-        console.log('lista', this.listaUsuarios);
-      } else {
-        this.listaUsuarios.pop();
-        this.toastVier("Usuario não Cadastrado", 3000);
-        console.log('lista', this.listaUsuarios);
-      }
-    });
-
-  }
-
-  //add Lista no BD
-  addLista() {
-    if (this.listaUsuarios[0].id == null) {
-      this.listaUsuarios.splice(0, 1, { nomeDisplay: this.usuario.displayName, email: this.usuario.email, id: this.usuario.uid });
-    } else {
-      this.listaUsuarios.push({ nomeDisplay: this.usuario.displayName, email: this.usuario.email, id: this.usuario.uid });
+  removeUserList(user:any, index:number){        
+    if(this.editList == true && user.id != this.user.uid){           
+      this.userList.splice(index, 1);
+      this.util.showToast("Usuário REMOVIDO da lista com sucesso", "bottom", 3000);
     }
-    let listaComIds = new Array();
-    this.listaUsuarios.forEach(element => {
-      listaComIds.push(element.id);
-    });
-    let lista = new Lista(this.nome_lista, listaComIds);
-    const id = this.bd.createId();
-    this.bd.collection('listas').doc(id).set(Object.assign({}, lista));
-    this.navCtrl.pop();
+    else if(this.editList == true && user.id == this.user.uid){
+      this.util.showToast("Você não pode Remover a si mesmo", "bottom", 3000); 
+    }
+    else{
+      this.listTemp.splice(index, 1)
+      this.util.showToast("Usuário REMOVIDO da lista com sucesso", "bottom", 3000);    
+    }       
   }
 
-  //toast mensagem
-  toastVier(msg: string, time: number) {
-    const toast = this.toastCtrl.create({
-      message: msg,
-      duration: time,
-      position: 'top'
-    });
-    toast.present();
-  }
-
+  
+  
 }
 
 
--- src/pages/model.ts
+++ src/pages/model.ts
@@ -11,7 +11,29 @@ export class Item {
         this.obs = obs;
         this.comprado = comprado;
     }
-
-
 }
+
+export class User {
+    id: string    
+    nomeDisplay: string
+    email: string    
+
+    constructor(id:string, nomeDisplay: string, email: string){
+        this.id = id;
+        this.nomeDisplay = nomeDisplay;
+        this.email = email;        
+    }
+}
+
+export class Lista {
+    nome_lista: String;
+    itens;
+    usuarios;
+  
+    constructor(nome_lista: string, usuarios: string[]) {
+      this.nome_lista = nome_lista;
+      this.itens = [];
+      this.usuarios = usuarios;
+    }
+  }
 
